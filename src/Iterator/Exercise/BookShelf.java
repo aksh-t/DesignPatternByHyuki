70dea36041d131f9b7e4e0de21da6c014bd5dba8
aksh-t <tanno.ao@gmail.com>
2020-04-19 15:29:40 +0900

    配列をArrayListに置き換え

--- src/Iterator/Exercise/BookShelf.java
+++ src/Iterator/Exercise/BookShelf.java
@@ -1,16 +1,19 @@
 package Exercise;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class BookShelf implements Aggregate {
-    private Book[] books;
+    private List<Book> books;
     private int last = 0;
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf() {
+        this.books = new ArrayList<Book>();
     }
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
     public void appendBook(Book book) {
-        this.books[last] = book;
+        this.books.add(last, book);
         last++;
     }
     public int getLength() {
