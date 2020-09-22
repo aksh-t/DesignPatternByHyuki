495b46285b6b68144b89ef26927dce8b3d459527
aksh-t <tanno.ao@gmail.com>
2020-09-23 00:04:56 +0900

    Builderをinterfaseに変更

---
+++ src/Builder/Exercise1/Builder.java
@@ -0,0 +1,11 @@
+package Builder.Exercise1;
+
+public interface Builder {
+    public void makeTitle(String title);
+
+    public void makeString(String str);
+
+    public void makeItems(String[] items);
+
+    public void close();
+}
