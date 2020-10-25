20930ea424d28ca524bb20c57ae4a567003c98b6
aksh-t <tanno.ao@gmail.com>
2020-10-26 02:37:13 +0900

    回答を写経

--- src/Bridge/Exercise3/Main.java
+++ src/Bridge/Exercise3/Main.java
@@ -2,12 +2,9 @@ package Bridge.Exercise3;
 
 public class Main {
     public static void main(String[] args) {
-        Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
-        Display d2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
-        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
-        d1.display();
-        d2.display();
-        d3.display();
-        d3.multiDisplay(5);
+        IncreaseDisplay d1 = new IncreaseDisplay(new CharDisplayImpl('<', '*', '>'), 1);
+        IncreaseDisplay d2 = new IncreaseDisplay(new CharDisplayImpl('|', '#', '-'), 2);
+        d1.increaseDisplay(4);
+        d2.increaseDisplay(6);
     }
 }
